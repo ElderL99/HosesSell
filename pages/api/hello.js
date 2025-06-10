26ac0fa70ad98bcfe1d0cb5fbb4f8a5e4b0f0bb1
ElderL99 <adan.lugo.barrientos@outlook.com>
2025-06-10 17:15:33 -0600

    primer commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,14 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
 
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  fetch(
+    "https://raw.githubusercontent.com/devchallenges-io/curriculum/refs/heads/main/4-frontend-libaries/challenges/group_1/data/property-listing-data.json"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      res.status(200).json(data); 
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Error al obtener datos" });
+    });
 }
